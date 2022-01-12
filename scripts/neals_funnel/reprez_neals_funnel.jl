d74fc0898c9e9005efad62c3557e99cadcfb2063
Theo Galy-Fajou <theo.galyfajou@gmail.com>
2022-01-12 14:36:43 +0100

    More intro updates

--- scripts/neals_funnel/reprez_neals_funnel.jl
+++ scripts/neals_funnel/reprez_neals_funnel.jl
@@ -1,3 +1,5 @@
+using Pkg
+Pkg.activate(@__DIR__)
 using CairoMakie
 using MeasureTheory
 using Soss
@@ -17,18 +19,18 @@ logdensity(m2(), (x=1, z=2))
 
 function plot_dist!(f, model; n=1000, xlabel="x", ylabel="z")
     xlin = LinRange(-5, 5, n)
-    ax = Axis(f[1,1], xlabel="x", ylabel="z", xlabelsize=30.0, ylabelsize=30.0, yticks=-5:1:5, xticks=-5:1:5)
+    ax = Axis(f[1,1]; xlabel, ylabel, xlabelsize=30.0, ylabelsize=30.0, yticks=-5:1:5, xticks=-5:1:5)
     contourf!(ax, xlin, xlin, (x,z)->exp(logdensity(model(), (;x, z))))
     resize_to_layout!(f)
     f |> display
 end
 
-fig_path = joinpath(@__DIR__, "..", "..", "thesis", "figures", "introduction")
+fig_path = joinpath(@__DIR__, "..", "..", "thesis", "chapters", "1_introduction", "figures")
 mkpath(fig_path)
 
 f = Figure(resolution=(600, 600));
 plot_dist!(f, m1)
 save(joinpath(fig_path, "neals_funnel_centered.pdf"), f)
 f = Figure(resolution=(600, 600));
-plot_dist!(f, m2; xlabel="x̃", ylabel="ỹ")
+plot_dist!(f, m2; xlabel="x̃", ylabel="z̃")
 save(joinpath(fig_path, "neals_funnel_non_centered.pdf"), f)
