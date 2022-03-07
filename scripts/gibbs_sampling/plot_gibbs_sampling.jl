0265860b4653edca456cc93350719f8d3fcb6646
Théo Galy-Fajou <theo.galyfajou@gmail.com>
2022-03-07 15:00:47 +0100

    Added a HMC example

--- scripts/gibbs_sampling/plot_gibbs_sampling.jl
+++ scripts/gibbs_sampling/plot_gibbs_sampling.jl
@@ -18,6 +18,7 @@ cg = ColorScheme(map(x->RGB(x...), include(joinpath(@__DIR__, "..", "colors", "c
 mc = :black
 
 const c = 1/20216.335877
+logp(x) = - (x[1]^2 * x[2]^2 + x[1]^2 + x[2]^2 - 8x[1] - 8x[2]) / 2
 p(x1, x2) = c * exp(- (x1^2 * x2^2 + x1^2 + x2^2 - 8x1 - 8x2) / 2)
 cond_p(x) = Normal(4 / (x^2 + 1), 1 / (x^2 + 1))
 
@@ -32,7 +33,7 @@ function plot_dist!(fig, f; n=1000, xlabel="x₁", ylabel="x₂")
     return ax
 end
 
-function create_path!(ax, n=10)
+function create_path_gibbs!(ax, n=10)
     x1 = randn(rng)
     x2 = randn(rng)
     xs = []
@@ -52,6 +53,44 @@ mkpath(fig_path)
 
 fig = Figure(resolution=(600, 600));
 ax = plot_dist!(fig, p)
-create_path!(ax, 20)
-save(joinpath(fig_path, "gibbs_sampling.pdf"), fig)
+create_path_gibbs!(ax, 20)
+# save(joinpath(fig_path, "gibbs_sampling.pdf"), fig)
+fig
+
+## 
+
+using ForwardDiff
+using LinearAlgebra
+grad(x) = ForwardDiff.gradient(x->-logp(x), x)
+function leapfrog(x, p, N, ϵ)
+    int_states = zeros(2, N+1)
+    for i in 1:N
+        p = p - ϵ / 2 * grad(x)
+        int_states[:, i] .= x
+        x = x + ϵ * p
+        p = p - ϵ / 2 * grad(x)
+    end
+    int_states[:, end] .= x
+    return x, int_states
+end
+function create_path_hmc!(ax, n=10, N=100, ϵ=1e-2)
+    x = randn(rng, 2)
+    xs = []
+    int_states = []
+    push!(xs, copy(x))
+    for i in 1:n
+        p = randn(2)
+        x, int_state = leapfrog(x, p, N, ϵ)
+        push!(xs, copy(x))
+        push!(int_states, int_state)
+    end
+    int_states = reduce(hcat, int_states)
+    lines!(ax, int_states[1, :], int_states[2, :], color=:grey)
+    scatter!(ax, first.(xs), last.(xs), color=mc)
+end
+
+fig = Figure(resolution=(600, 600));
+ax = plot_dist!(fig, p)
+create_path_hmc!(ax, 20)
+save(joinpath(fig_path, "hmc_sampling.pdf"), fig)
 fig
