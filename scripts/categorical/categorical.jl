7476d8f53b4b79428ca5124cfdcb1999b0c13256
Théo Galy-Fajou <theo.galyfajou@gmail.com>
2022-03-17 15:32:35 +0100

    Added new figures

--- scripts/categorical/categorical.jl
+++ scripts/categorical/categorical.jl
@@ -91,7 +91,7 @@ function gibbs_sample(fz, lik, Y, fs, Ω; nsamples=200)
     end
 end;
 # We initialize our random variables
-samples = map(liks, Ys, plts) do lik, Y, plt
+samples = map(liks, Ys) do lik, Y
     fs_init = nestedview(randn(N, nlatent(lik)))
     Ω = init_aux_variables(lik, N);
     # Run the sampling for default number of iterations (200)
@@ -104,104 +104,109 @@ end;
 ## 
 sb = ColorSchemes.seaborn_colorblind
 GPtheme = Theme(palette=sb, linewidth=10.0)
-fig = Figure(resolution=(900, 600))
-lw = 3.0
+
+tit = ["K-1", "K"]
+isbij = ["_bijective", "_nonbijective"]
 for i in 1:2
+    fig = Figure(resolution=(900, 600))
+    lw = 4.0
     with_theme(GPtheme, linewidth=3.0) do 
-        ax1 = Axis(fig[1, 2], title=L"y | f, g", titlesize=25.0)
+        ax1 = Axis(fig[1, 2], title=L"""y |\left(f_j\right)""", titlesize=25.0)
         ax2 = Axis(fig[1, 3], title="Latent GPs", titlesize=19.0)
         ax3 = Axis(fig[2, 2], title="")
         ax4 = Axis(fig[2, 3], title="")
 
-        # Plot the truth
-        scatter!(ax1, x, y / Nclass; color=sb[y], label=[1 2 3 4], msw=0.0)
-        ps_true = getproperty.(lik_true.v, :p)
-        for k in 1:Nclass
-            lines!(ax1, x, invert(ps_true)[k], color=sb[k], lw =3.0)
-            lines!(ax3, x, invert(ps_true)[k], color=sb[k], lw =3.0)
-        end
-
-        for f in vcat(fs, [zeros(N)])
-            lines!(ax2, x, f, color=sb[k], lw=3.0)
-            lines!(ax4, x, f, color=sb[k], lw=3.0)
-        end
-
         # Plot variational and sampling results
-        lik_pred = liks[i](invert(mean.([u_post(x) for u_post in u_posterior.(Ref(fz), ms_Ss[i].m, ms_Ss[i].S)])))
+        u_posts = [u_post(x) for u_post in u_posterior.(Ref(fz), ms_Ss[i].m, ms_Ss[i].S)]
+        lik_pred = liks[i](invert(mean.(u_posts)))
         ps_pred = getproperty.(lik_pred.v, :p)
         samp_pred = map(samples[i]) do s
-            getproperty(lik(invert(s)).v, :p)
+            getproperty.(liks[i](invert(s)).v, :p)
         end
-        α = 1e-3
+        α = 8e-2
         for k in 1:Nclass
-            lines!(ax1, x, invert(ps_true)[k], color=sb[k], lw =3.0)
+            lines!(ax1, x, invert(ps_pred)[k], color=sb[k], linewidth=lw, linestyle=:dash)
             for s_p in samp_pred
-                lines!(ax3, x, invert(ps_true)[k], color=(sb[k], α), lw =3.0)
+                lines!(ax3, x, invert(s_p)[k], color=(sb[k], α))
             end
         end
 
-
-        y_qσ = sqrt.(lik.invlink.(mean(g_te)))
-        band!(ax1, x_te, y_qm - y_qσ, y_qm + y_qσ, color=(sb[3], 0.5))
-        lines!(ax1, x_te, y_qm, label=L"E_{q(f,g)}[p(y|f,g)]", linewidth=3.0, color=sb[3])
-        
-        plot!(ax2, f_te, color=(sb[1], 0.3), linestyle=:dash, label=L"q(f)", linewidth=lw)
-        plot!(ax2, g_te, color=(sb[2], 0.3), linestyle=:dash, label=L"q(g)", linewidth=lw)
-        
-        alpha = 1e-2
-        for fs in fs_samples
-            fσ = sqrt.(lik.invlink.(fs[2]))
-            band!(ax3, x, fs[1] - fσ, fs[1] + fσ, color=(sb[3], alpha))
-            lines!(ax3, x, fs[1], color=(sb[3], alpha * 10))
-            for i in 1:nlatent(lik)
-                plot!(ax4, x, fs[i]; color=(sb[i], 0.01), lw=1.0)
+        for (l, u) in enumerate(u_posts)
+            plot!(ax2, u, linestyle=:dash, color=(sb[l], 0.5), linewidth=lw)
+        end
+        for fs in samples[i]
+            for (k, f) in enumerate(fs)
+                lines!(ax4, x, f, color=(sb[k], α), linewidth=1.0)
             end
         end
-
-        
-        y_σ = sqrt.(lik.invlink.(fs[2]))
-        lines!(ax1, x, fs[1] - y_σ, color=(sb[1], 0.5), linestyle=:dash, linewidth=lw)
-        lines!(ax1, x, fs[1] + y_σ, color=(sb[1], 0.5), linestyle=:dash, linewidth=lw)
-        # band!(ax3, x, fs[1] - y_σ, fs[1] + y_σ, color=(sb[1], 0.5))
-        lines!(ax1, x, fs[1], label=L"p(y|f,g)", linewidth=3.0, color=sb[1])
-        lines!(ax3, x, fs[1] - y_σ, color=(sb[1], 0.5), linestyle=:dash, linewidth=lw)
-        lines!(ax3, x, fs[1] + y_σ, color=(sb[1], 0.5), linestyle=:dash, linewidth=lw)
-        lines!(ax3, x, fs[1], label=L"p(y|f,g)", linewidth=3.0, color=sb[1])
-        
-        lines!(ax2, x, fs[1], label=L"f", color=sb[1], linewidth=lw)
-        lines!(ax4, x, fs[1], label=L"f", color=sb[1], linewidth=lw)
-        lines!(ax2, x, fs[2], label=L"g", color=sb[2], linewidth=lw)
-        lines!(ax4, x, fs[2], label=L"g", color=sb[2], linewidth=lw)
-        
-        scatter!(ax1, x, y; label="y", color=sb[2], markersize=6.0)
-        scatter!(ax3, x, y; label="y", color=sb[2], markersize=6.0)
+        ps_true = getproperty.(lik_true.v, :p)
+        # Plot the true probability
+        for k in 1:Nclass
+            lines!(ax1, x, invert(ps_true)[k], color=sb[k], linewidth=lw)
+            lines!(ax3, x, invert(ps_true)[k], color=sb[k], linewidth=lw)
+        end
+        # Plot the true latent
+        for (k, f) in enumerate(vcat(fs, [zeros(N)]))
+            lines!(ax2, x, f, color=sb[k], linewidth=lw)
+            lines!(ax4, x, f, color=sb[k], linewidth=lw)
+        end
+        scatter!(ax1, x, y/Nclass; color=sb[y], label=[1 2 3 4], msw=0.0)
+        scatter!(ax3, x, y/Nclass; color=sb[y], label=[1 2 3 4], msw=0.0)
         tightlimits!.((ax1, ax2, ax3, ax4))
+        ylims!(ax1, (0, 1.5))
         linkyaxes!(ax1, ax3)
         linkyaxes!(ax2, ax4)
 
-        elem_1 = [
-            LineElement(color = (sb[1], 0.5), linestyle=:dash, points = Point2f[(0, 1), (1,1)]),
-            LineElement(color = sb[1], linewidth=lw),
-            LineElement(color = (sb[1], 0.5), linestyle=:dash, points = Point2f[(0, 0), (1,0)]),
-        ]
+        elem_1 = MarkerElement(color = sb[1:3], marker=:circle, points = Point2f[(0.2, 0.2), (0.5, 0.8), (0.8, 0.2)])
         elem_2 = [
-            LineElement(color = sb[3], linewidth=lw),
-            PolyElement(color = (sb[3], 0.5), strokewidth=0.0)
+        LineElement(color = sb[1], linewidth=lw, points = Point2f[(0, 1), (1,1)]),
+        LineElement(color = sb[2], linewidth=lw),
+        LineElement(color = sb[3], linewidth=lw, points = Point2f[(0, 0), (1,0)]),
         ]
-        elem_3 = MarkerElement(color=sb[2], marker=:circle)
+        elem_3 = [
+        LineElement(color = sb[1], linestyle=:dash, linewidth=2.0, points = Point2f[(0, 1), (1,1)]),
+        LineElement(color = sb[2], linestyle=:dash, linewidth=2.0),
+        LineElement(color = sb[3], linestyle=:dash, linewidth=2.0, points = Point2f[(0, 0), (1,0)]),
+        ]
         elem_4 = [
-            LineElement(color = (sb[1], 0.5), points = Point2f[(0, 2//3), (1, 2//3)], linewidth=lw),
-            LineElement(color = (sb[2], 0.5), points = Point2f[(0, 1//3), (1, 1//3)], linewidth=lw),
+            LineElement(color = sb[1], linewidth=2.0, linestyle=:dash, points = Point2f[(0, 0.8), (1,0.8)]),
+            PolyElement(color = (sb[1], 0.5), strokewidth=0.0, points = Point2f[(0, 0.66), (1, 0.66), (1, 1), (0, 1)]),
+            LineElement(color = sb[2], linewidth=2.0, linestyle=:dash),
+            PolyElement(color = (sb[2], 0.5), strokewidth=0.0, points = Point2f[(0, 0.33), (1, 0.33), (1, 0.66), (0, 0.66)]),
+            LineElement(color = sb[3], linewidth=2.0, linestyle=:dash, points = Point2f[(0, 0.2), (1,0.2)]),
+            PolyElement(color = (sb[3], 0.5), strokewidth=0.0, points = Point2f[(0, 0), (1, 0), (1, 0.33), (0, 0.33)])
         ]
-        Legend(fig[1, 2], [ elem_3, elem_1, elem_2], [L"y", L"p(y|f,g)", L"E_{q(f,g)}[p(y|f,g)]"], tellwidth=false, halign=:right, valign=:top, margin=(10, 10, 10, 10))
-        Legend(fig[2, 2], [elem_2],  [L"""\left(p(y|f_i,g_i)\right)_{i=1}^S"""], tellwidth=false, halign=:right, valign=:top, margin=(10, 10, 10, 10))
-        Legend(fig[2, 3], [elem_4], [L"""\left(f_i,g_i\right)_{i=1}^S\sim p(f,g|y)"""],tellwidth=false, halign=:right, valign=:top, margin=(10, 10, 10, 10))
+        # elem_3 = MarkerElement(color=sb[2], marker=:circle)
+        # elem_4 = [
+        #     LineElement(color = (sb[1], 0.5), points = Point2f[(0, 2//3), (1, 2//3)], linewidth=lw),
+        #     LineElement(color = (sb[2], 0.5), points = Point2f[(0, 1//3), (1, 1//3)], linewidth=lw),
+        # ]
+        leg_labels = [L"y", L"p(y=k|(f_j))", L"E_{q(f_j)}[p(y=k|(f_j))]"]
+        Legend(fig[1, 2], [elem_1, elem_2, elem_3], leg_labels, tellwidth=false, halign=:center, valign=:top, margin=(2, 2, 2, 2), nbanks=3, framevisible=false)
+        Legend(fig[1, 3], [elem_2, elem_4], [L"(f_j)", L"(q(f_j))"], tellwidth=false, framevisible=false, nbanks=2, valign=:top, halign=:center)
+        Legend(fig[2, 2], [elem_2], [L"""\left((p|(f_j)^s)\right)_{s=1}^S"""], tellwidth=false, halign=:center, valign=:top, margin=(2, 2, 2, 2), nbanks=3, framevisible=false)
+        Legend(fig[2, 3], [elem_2], [L"""\left((f_j)^s\right)_{s=1}^S"""], tellwidth=false, halign=:center, valign=:top, margin=(2, 2, 2, 2), nbanks=3, framevisible=false)
+        
+        # Legend(fig[2, 2], [elem_2],  [L"""\left(p(y|f_i,g_i)\right)_{i=1}^S"""], tellwidth=false, halign=:right, valign=:top, margin=(10, 10, 10, 10))
+        # Legend(fig[2, 3], [elem_4], [L"""\left(f_i,g_i\right)_{i=1}^S\sim p(f,g|y)"""],tellwidth=false, halign=:right, valign=:top, margin=(10, 10, 10, 10))
         # axislegend(ax1)
-        axislegend(ax2, nbanks=2)
+        # axislegend(ax2, nbanks=2)
+
+        # Plot the truth
+
+
         Label(fig[1, 1], text="Variational\nInference", textsize=20, rotation = pi/2, tellheight=false)
         Label(fig[2, 1], text="Gibbs\nSampling", textsize=20, rotation = pi/2, tellheight=false)
         fig_path = joinpath(@__DIR__, "..", "..", "thesis", "chapters", "8_discussions", "figures")
-        save(joinpath(fig_path, "categorical.pdf"), fig)
+        save(joinpath(fig_path, "categorical" * isbij[i] * ".pdf"), fig)
         display(fig)
     end
 end
+
+## 
+
+fig = Figure()
+# ax = Axis(fig[1,1], title= latexstring("{\$y_i\$}\$\\phantom{I}_{i=1}^N\$"))
+# ax = Axis(fig[1,1], title=latexstring("an equation:{\$1 + \\alpha^2\$}"))
+
+fig
